77d9721d14d5f10212b5ae333eacff2df99528fe
Justin Naing <justin.aung19@gmail.com>
2017-06-10 16:41:25 +0700

    config for deployment

--- project/_config.py
+++ project/_config.py
@@ -7,6 +7,7 @@ DATABASE = 'flasktaskr.db'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = b'w\n\xb1_\x88F0\x19\xaa\xb5M\x84!\xb8\xe7\xf2_\x8d\xa6\xb1\xbe\xe6\x7f8'
 WTF_CSRF_SECRET_KEY = b'w\n\xb1_\x88F0\x19\xaa\xb5M\x84!\xb8\xe7\xf2_\x8d\xa6\xb1\xbe\xe6\x7f8'
+DEBUG = False
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
