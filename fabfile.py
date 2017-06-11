d46dafd06b0b5e4aaebb4f6a1dca821de09abf0f
Justin Naing <justin.aung19@gmail.com>
2017-06-11 17:09:56 +0700

    good to go

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,12 @@
-from fabric.api import local
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
 
 
 def test():
-    local('nosetests -v')
+    with settings(warn_only=True):
+        result = local('nosetests -v', capture=True)
+    if result.failed and not confirm('Tests failed. Continue?'):
+        abort('Aborted at user request.')
 
 
 def commit():
@@ -18,3 +22,33 @@ def prepare():
     test()
     commit()
     push()
+
+
+# deploy
+
+
+def pull():
+    local('git pull origin master')
+
+
+def heroku():
+    local('git push heroku master')
+
+
+def heroku_test():
+    local('heroku run nosetests -v')
+
+
+def deploy():
+    pull()
+    test()
+    commit()
+    heroku()
+    heroku_test()
+
+
+# rollback
+
+
+def rollback():
+    local('heroku rollback')
